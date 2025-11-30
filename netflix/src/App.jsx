b6f99406bff8bd44b564185a333727519c464691
alem624 <alemtshayatreso@gmail.com>
2025-11-30 21:12:34 +0300

    axios and request

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/Home/Home'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>hello</p>
+      <Home />
+      
     </>
   )
 }
